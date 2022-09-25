c2652cee1724d56f4ff6d88c7ca944dea826d015
sanghaviguptaa <114340527+sanghaviguptaa@users.noreply.github.com>
2022-09-25 17:46:49 +0530

    Add files via upload

---
+++ pgm_27 Stepper motor/stepper_motor/stepper_motor.ino
@@ -0,0 +1,35 @@
+/*Interfacing Stepper motor with Arduino Uno 
+ * 
+ * Hardware components
+ * Arduino Uno 
+ * L298 - Driver
+ * stepper motor - Annimated Bipolar Stepper motor
+ * 
+ * Author : Sanghavi 
+ * Date : 25-09-2022
+ */
+ 
+#include <Stepper.h>
+
+// 8 steps are taken here to complete 360degrees in 45 degree angle 
+const int stepsPerRevolution = 8;  // change this to fit the number of steps per revolution
+// for your motor
+
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+
+void setup() {
+  // set the speed at 60 rpm:
+  myStepper.setSpeed(10);
+  
+}
+
+void loop() {
+  // step one revolution  in one direction: 
+  myStepper.step(stepsPerRevolution);
+  delay(500);
+
+  // step one revolution in the other direction:
+  myStepper.step(-stepsPerRevolution);
+  delay(500);
+}
